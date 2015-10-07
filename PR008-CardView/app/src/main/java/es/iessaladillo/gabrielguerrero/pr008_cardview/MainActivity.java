c4e3a525538b2a751c0579e79e497907c94b61f7
GabriL0L <gabgue94@gmail.com>
2015-10-07 18:00:33 +0200

    pr008 acabado

--- PR008-CardView/app/src/main/java/es/iessaladillo/gabrielguerrero/pr008_cardview/MainActivity.java
+++ PR008-CardView/app/src/main/java/es/iessaladillo/gabrielguerrero/pr008_cardview/MainActivity.java
@@ -4,34 +4,107 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText cuenta;
+    private EditText porPropina;
+    private EditText propina;
+    private EditText total;
+    private Button calcularCuenta;
+    private Button limpiarCuenta;
+    private Button redondearCuenta;
+    private EditText comensal;
+    private EditText porComensal;
+    private Button calcularComensal;
+    private Button limpiarComensal;
+    private Button redondearComensal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void initViews(){
+        cuenta = (EditText) findViewById(R.id.cuenta);
+        porPropina = (EditText) findViewById(R.id.porPropina);
+        propina = (EditText) findViewById(R.id.propina);
+        total = (EditText) findViewById(R.id.total);
+        calcularCuenta = (Button) findViewById(R.id.calcularCuenta);
+        limpiarCuenta = (Button) findViewById(R.id.limpiarCuenta);
+        redondearCuenta = (Button) findViewById(R.id.redondearCuenta);
+        comensal = (EditText) findViewById(R.id.comensal);
+        porComensal = (EditText) findViewById(R.id.porComensal) ;
+        calcularComensal = (Button) findViewById(R.id.calcularComensal);
+        limpiarComensal = (Button) findViewById(R.id.limpiarComensal);
+        redondearComensal = (Button) findViewById(R.id.redondearComensal);
+
+        calcularCuenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float prop = Float.valueOf(cuenta.getText().toString()) * (Float.valueOf(porPropina.getText().toString())/100);
+                propina.setText(String.format(Locale.getDefault(),".2f",prop));
+                total.setText(String.format(Locale.getDefault(),".2f",(Float.valueOf(cuenta.getText().toString())+prop)));
+            }
+        });
+
+        limpiarCuenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cuenta.setText("");
+                porPropina.setText("");
+                propina.setText("");
+                total.setText("");
+            }
+        });
+
+        redondearCuenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float ctotal = Float.valueOf(total.getText().toString());
+                float nuevoTotal = (float) Math.floor(ctotal);
+                if(nuevoTotal != ctotal){
+                    nuevoTotal += 1;
+                }
+                total.setText(String.format(Locale.getDefault(),".2f",nuevoTotal));
+            }
+        });
+
+        calcularComensal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float cambio;
+                cambio = Float.valueOf(total.getText().toString()) / Float.valueOf(comensal.getText().toString());
+                porComensal.setText(String.format(Locale.getDefault(),".2f",cambio));
+            }
+        });
+
+        limpiarComensal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                comensal.setText("");
+                porComensal.setText("");
+            }
+        });
+
+        redondearComensal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float ctotal = Float.valueOf(porComensal.getText().toString());
+                float nuevoTotal = (float) Math.floor(ctotal);
+                if(nuevoTotal != ctotal){
+                    nuevoTotal += 1;
+                }
+                porComensal.setText(String.format(Locale.getDefault(),".2f",nuevoTotal));
+            }
+        });
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
